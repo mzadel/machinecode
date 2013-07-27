f1158e631374936fb1c87acb809f84f213a04ecf
Mark Zadel <zadel@music.mcgill.ca>
2013-07-27 18:47:24 -0400

    CodeAst: made the field and instruction fields more generic

--- CodeAst.hs
+++ CodeAst.hs
@@ -7,19 +7,14 @@ module CodeAst where
 
 import Data.Bit
 
--- this should be an instance of a typeclass that I can use to write a
--- polymorphic function on instructions
--- this is maybe the only thing the end user needs to define
 data DcpuFieldType = DcpuLiteralBits | DcpuRegA | DcpuRegB | DcpuOptionalWord
     deriving (Show)
 
--- this can be generic, not specific to dcpu
 -- field type, its payload (string of bits), interpretation
-data DcpuField = DcpuField DcpuFieldType [Bit] String
+data Field a = Field a [Bit]
     deriving (Show)
 
--- this can be generic, not specific to dcpu
-data DcpuInstruction = DcpuInstruction String [DcpuField]
+data Instruction a = Instruction a [Field a]
     deriving (Show)
 
 -- vim:sw=4:ts=4:et:ai:
