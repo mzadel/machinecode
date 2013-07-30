1b4b5312d99bffc98851d4b47943e26fc918ad14
Mark Zadel <zadel@music.mcgill.ca>
2013-07-30 18:35:01 -0400

    CodeAst: update the Field type
    embed the literal/variable distinction in the type constructor

--- CodeAst.hs
+++ CodeAst.hs
@@ -7,13 +7,7 @@ module CodeAst where
 
 import Data.Bit
 
-data FieldType = Literal | Variable
-    deriving (Show)
-
--- maybe instead have a FieldLiteral type constructor and a FieldVariable type
--- constructor, so the literal one doesn't have to know about a and so it won't
--- have to be specified in the covert function
-data Field a = Field FieldType a [Bit]
+data Field a = FieldLiteral [Bit] | FieldVariable a [Bit] 
     deriving (Show)
 
 data Instruction a b = Instruction a [Field b]
