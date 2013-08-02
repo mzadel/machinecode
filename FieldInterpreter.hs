8c3d8e1576d24cbd80696c4eb469c171b2409b2e
Mark Zadel <zadel@music.mcgill.ca>
2013-08-02 18:00:05 -0400

    FieldInterpreter: return the first match
    this'll lead to an error if there's no match
    
    also if there's more than one match, it'll just give you the first one

--- FieldInterpreter.hs
+++ FieldInterpreter.hs
@@ -9,9 +9,10 @@ import Data.Bit
 
 -- take a spec string and the bits found in them, and return an interpretation
 -- based on the lookup table
-fieldinterpreter :: [( String, [Bit], a )] -> String -> [Bit] -> [a]
-fieldinterpreter table specstring parsedbits = [ l | s <- strings, b <- bits, l <- labels, s == specstring, b == parsedbits ]
+fieldinterpreter :: [( String, [Bit], a )] -> String -> [Bit] -> a
+fieldinterpreter table specstring parsedbits = head matchinglabels
     where
         (strings,bits,labels) = unzip3 table
+        matchinglabels = [ l | s <- strings, b <- bits, l <- labels, s == specstring, b == parsedbits ]
 
 -- vim:sw=4:ts=4:et:ai:
