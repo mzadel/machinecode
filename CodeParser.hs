b2b1ae9adbe447c9a63d64ad99e51e0052386315
Mark Zadel <zadel@music.mcgill.ca>
2013-08-02 18:32:57 -0400

    CodeParser: integrated FieldInterpreter code

--- CodeParser.hs
+++ CodeParser.hs
@@ -3,7 +3,6 @@
 -- CodeParser.hs
 --
 
-import FieldInterpreter -- move this code here afterward
 import SpecAstToCodeParser
 import SpecParser
 import qualified SpecAst as S
@@ -11,6 +10,14 @@ import qualified CodeAst as C
 import Data.Either (rights)
 import Text.Parsec.Prim
 import Data.Bit
+
+-- take a spec string and the bits found in them, and return an interpretation
+-- based on the lookup table
+fieldinterpreter :: [( String, [Bit], a )] -> String -> [Bit] -> a
+fieldinterpreter table specstring parsedbits = head matchinglabels
+    where
+        (strings,bits,labels) = unzip3 table
+        matchinglabels = [ l | s <- strings, b <- bits, l <- labels, s == specstring, b == parsedbits ]
 
 codeparser :: [(String,String)] -> (String->[Bit]->a) -> Parser [C.Instruction String a]
 codeparser instrspecs convert = many $ instructionparser
