e7351e9b57e77166faf5076982e98586959df5f3
Mark Zadel <zadel@music.mcgill.ca>
2013-08-02 18:17:42 -0400

    CodeParser: I'm going to move the code parser code here

--- CodeParser.hs
+++ CodeParser.hs
@@ -3,4 +3,18 @@
 -- CodeParser.hs
 --
 
+import FieldInterpreter
+import SpecAstToCodeParser
+import qualified SpecAst as S
+import qualified CodeAst as C
+
+codeparser :: Parser [Code.Instruction String a]
+codeparser = many $ instructionparser
+    where
+        specasts = rights [ specToAst spec label | (spec,label) <- instrspecs ]
+        instructionparser = specsToParser convert specasts
+
+
+
+
 -- vim:sw=4:ts=4:et:ai:
