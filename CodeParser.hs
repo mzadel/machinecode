ba8786b65929bfe62397088749f963984d24ea39
Mark Zadel <zadel@music.mcgill.ca>
2013-08-03 11:50:37 -0400

    CodeParser: set up to ignore "extra field" column of the table
    now everything compiles

--- CodeParser.hs
+++ CodeParser.hs
@@ -15,15 +15,15 @@ import Data.Bit
 
 -- take a spec string and the bits found in them, and return an interpretation
 -- based on the lookup table
-fieldlabeler :: [( String, [Bit], a )] -> String -> [Bit] -> a
+fieldlabeler :: [( String, [Bit], a, String )] -> String -> [Bit] -> a
 fieldlabeler table specstring parsedbits = head matchinglabels
     where
         matchinglabels = map extractlabel matchingtuples
-        extractlabel (s,b,l) = l
+        extractlabel (s,b,l,x) = l
         matchingtuples = filter matches table
-        matches (s,b,l) = s == specstring && b == parsedbits
+        matches (s,b,l,x) = s == specstring && b == parsedbits
 
-codeparser :: [(String,String)] -> [( String, [Bit], a )] -> Parser [C.Instruction String a]
+codeparser :: [(String,String)] -> [( String, [Bit], a, String )] -> Parser [C.Instruction String a]
 codeparser instrspecs fieldlabeltable = many $ instructionparser
     where
         specasts = rights [ specToAst spec label | (spec,label) <- instrspecs ]
