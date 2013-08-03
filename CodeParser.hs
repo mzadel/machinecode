f4c869d50218a0eca003abeef350cdfde38d363a
Mark Zadel <zadel@music.mcgill.ca>
2013-08-03 13:46:13 -0400

    CodeParser: change type vars to u to match SpecAstToCodeParser
    also match Parser def'n from SpecAstToCodeParser

--- CodeParser.hs
+++ CodeParser.hs
@@ -13,28 +13,28 @@ import Data.Either (rights)
 import Text.Parsec.Prim
 import Data.Bit
 
-matchtableentries :: [( String, [Bit], a, b->b )] -> String -> [Bit] -> [( String, [Bit], a, b->b )]
+matchtableentries :: [( String, [Bit], a, u->u )] -> String -> [Bit] -> [( String, [Bit], a, u->u )]
 matchtableentries table specstring parsedbits = filter ismatch table
     where
         ismatch (s,b,_,_) = s == specstring && (b == parsedbits || null parsedbits)
 
 -- take a spec string and the bits found in them, and return a label
 -- based on the lookup table
-fieldlabeler :: [( String, [Bit], a, b->b )] -> String -> [Bit] -> a
+fieldlabeler :: [( String, [Bit], a, u->u )] -> String -> [Bit] -> a
 fieldlabeler table specstring parsedbits = head matchinglabels
     where
         matchinglabels = map extractlabel matchingtuples
         extractlabel (_,_,l,_) = l
         matchingtuples = matchtableentries table specstring parsedbits
 
-getstatetransform :: [( String, [Bit], a, b->b )] -> String -> [Bit] -> (b -> b)
+getstatetransform :: [( String, [Bit], a, u->u )] -> String -> [Bit] -> (u -> u)
 getstatetransform table specstring parsedbits = head matchingstatetransforms
     where
         matchingstatetransforms = map extractstatetransform matchingtuples
         extractstatetransform (_,_,_,f) = f
         matchingtuples = matchtableentries table specstring parsedbits
 
-codeparser :: [(String,String)] -> [( String, [Bit], a, b->b )] -> Parser [C.Instruction String a]
+codeparser :: [(String,String)] -> [( String, [Bit], a, u->u )] -> Parser u [C.Instruction String a]
 codeparser instrspecs fieldlabeltable = many $ instructionparser
     where
         specasts = rights [ specToAst spec label | (spec,label) <- instrspecs ]
