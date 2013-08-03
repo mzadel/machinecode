cabbd70510970510e9f150487d160883880bc517
Mark Zadel <zadel@music.mcgill.ca>
2013-08-03 14:07:46 -0400

    propagate shouldparse function through CodeParser and SpecAstToCodeParser

--- CodeParser.hs
+++ CodeParser.hs
@@ -34,11 +34,11 @@ getstatetransform table specstring parsedbits = head matchingstatetransforms
         extractstatetransform (_,_,_,f) = f
         matchingtuples = matchtableentries table specstring parsedbits
 
-codeparser :: [(String,a)] -> [( String, [Bit], b, u->u )] -> Parser u [C.Instruction a b]
-codeparser instrspecs fieldlabeltable = many $ instructionparser
+codeparser :: [(String,a)] -> [( String, [Bit], b, u->u )] -> (String -> u -> Bool) -> Parser u [C.Instruction a b]
+codeparser instrspecs fieldlabeltable shouldparse = many $ instructionparser
     where
         specasts = rights [ specToAst spec label | (spec,label) <- instrspecs ]
-        instructionparser = specsToParser labeler statetransformer specasts
+        instructionparser = specsToParser labeler statetransformer shouldparse specasts
         labeler = fieldlabeler fieldlabeltable
         statetransformer = getstatetransform fieldlabeltable
 
--- SpecAstToCodeParser.hs
+++ SpecAstToCodeParser.hs
@@ -32,13 +32,13 @@ bitSpecToParser bitchars = sequence (map bitchartoparser bitchars)
 
 -- signature for the function to convert a spec field to a code field
 -- spec field, (field string -> parsed contents -> output field type) -> Parser for that field type
-specFieldToCodeFieldParser :: (String -> [Bit] -> a) -> (String -> [Bit] -> (u -> u)) -> S.Field -> Parser u (C.Field a)
+specFieldToCodeFieldParser :: (String -> [Bit] -> a) -> (String -> [Bit] -> (u -> u)) -> (String -> u -> Bool) -> S.Field -> Parser u (C.Field a)
 
-specFieldToCodeFieldParser convert statetransformer (S.FieldLiteral specstring) = do
+specFieldToCodeFieldParser convert _ _ (S.FieldLiteral specstring) = do
     bitsparsed <- bitSpecToParser specstring
     return (C.FieldLiteral bitsparsed)
 
-specFieldToCodeFieldParser convert statetransformer (S.FieldVariable specstring) = do
+specFieldToCodeFieldParser convert statetransformer shouldparse (S.FieldVariable specstring) = do
     bitsparsed <- bitSpecToParser specstring
     modifyState (statetransformer specstring bitsparsed)
     return (C.FieldVariable (convert specstring bitsparsed) bitsparsed)
@@ -47,12 +47,12 @@ specFieldToCodeFieldParser convert statetransformer (S.FieldVariable specstring)
 -- convert one spec to a parser for that spec
 -- NB: we're hardcoding the first field of CodeAst.InstructionSpec (the
 -- description) to be a string here.  Not sure how I'd do it otherwise yet.
-specToParser :: (String -> [Bit] -> b) -> (String -> [Bit] -> (u -> u)) -> S.InstructionSpec a -> Parser u (C.Instruction a b)
-specToParser convert statetransformer (S.InstructionSpec name fields) = do
-    parsedfields <- sequence $ map (specFieldToCodeFieldParser convert statetransformer) fields
+specToParser :: (String -> [Bit] -> b) -> (String -> [Bit] -> (u -> u)) -> (String -> u -> Bool) -> S.InstructionSpec a -> Parser u (C.Instruction a b)
+specToParser convert statetransformer shouldparse (S.InstructionSpec name fields) = do
+    parsedfields <- sequence $ map (specFieldToCodeFieldParser convert statetransformer shouldparse) fields
     return (C.Instruction name parsedfields)
 
-specsToParser :: (String -> [Bit] -> b) -> (String -> [Bit] -> (u -> u)) -> [S.InstructionSpec a] -> Parser u (C.Instruction a b)
-specsToParser convert statetransformer specs = choice $ map (try . specToParser convert statetransformer) specs
+specsToParser :: (String -> [Bit] -> b) -> (String -> [Bit] -> (u -> u)) -> (String -> u -> Bool) -> [S.InstructionSpec a] -> Parser u (C.Instruction a b)
+specsToParser convert statetransformer shouldparse specs = choice $ map (try . specToParser convert statetransformer shouldparse) specs
 
 -- vim:sw=4:ts=4:et:ai:
