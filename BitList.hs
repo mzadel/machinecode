6697a23bb83bb29caa1179fc2a325a4f6cfe1d10
Mark Zadel <zadel@music.mcgill.ca>
2013-08-04 15:00:02 -0400

    Pretty: moved tohex back in
    since it's specific to pretty printing

--- BitList.hs
+++ BitList.hs
@@ -62,29 +62,4 @@ bitsToWord16 [b15,b14,b13,b12,b11,b10,b9,b8,b7,b6,b5,b4,b3,b2,b1,b0] =
     fromIntegral b1  * 0x0002 +
     fromIntegral b0  * 0x0001
 
-tohex :: [Bit] -> String
-tohex bs = concat $ map onechar (byfours bs)
-    where
-        byfours :: [Bit] -> [[Bit]]
-        byfours [] = []
-        byfours bs = a : byfours b
-            where
-                (a,b) = splitAt 4 bs
-        onechar (0:0:0:0:bs) = "0   "
-        onechar (0:0:0:1:bs) = "1   "
-        onechar (0:0:1:0:bs) = "2   "
-        onechar (0:0:1:1:bs) = "3   "
-        onechar (0:1:0:0:bs) = "4   "
-        onechar (0:1:0:1:bs) = "5   "
-        onechar (0:1:1:0:bs) = "6   "
-        onechar (0:1:1:1:bs) = "7   "
-        onechar (1:0:0:0:bs) = "8   "
-        onechar (1:0:0:1:bs) = "9   "
-        onechar (1:0:1:0:bs) = "a   "
-        onechar (1:0:1:1:bs) = "b   "
-        onechar (1:1:0:0:bs) = "c   "
-        onechar (1:1:0:1:bs) = "d   "
-        onechar (1:1:1:0:bs) = "e   "
-        onechar (1:1:1:1:bs) = "f   "
-
 -- vim:sw=4:ts=4:et:ai:
--- Pretty.hs
+++ Pretty.hs
@@ -9,12 +9,37 @@ import CodeAst
 import Data.Bit
 import Data.Word (Word16)
 import Data.Bits
-import BitList (bitstostring,bitsToWord16,tohex)
+import BitList (bitstostring,bitsToWord16)
 
 -- given the code ast for one instruction, return a string that pretty prints it
 -- todo: split out the generic parts of this from the dcpu-specific parts
 
 labelcolumn = 50
+
+tohex :: [Bit] -> String
+tohex bs = concat $ map onechar (byfours bs)
+    where
+        byfours :: [Bit] -> [[Bit]]
+        byfours [] = []
+        byfours bs = a : byfours b
+            where
+                (a,b) = splitAt 4 bs
+        onechar (0:0:0:0:bs) = "0   "
+        onechar (0:0:0:1:bs) = "1   "
+        onechar (0:0:1:0:bs) = "2   "
+        onechar (0:0:1:1:bs) = "3   "
+        onechar (0:1:0:0:bs) = "4   "
+        onechar (0:1:0:1:bs) = "5   "
+        onechar (0:1:1:0:bs) = "6   "
+        onechar (0:1:1:1:bs) = "7   "
+        onechar (1:0:0:0:bs) = "8   "
+        onechar (1:0:0:1:bs) = "9   "
+        onechar (1:0:1:0:bs) = "a   "
+        onechar (1:0:1:1:bs) = "b   "
+        onechar (1:1:0:0:bs) = "c   "
+        onechar (1:1:0:1:bs) = "d   "
+        onechar (1:1:1:0:bs) = "e   "
+        onechar (1:1:1:1:bs) = "f   "
 
 fieldbits :: Field a -> [Bit]
 fieldbits (FieldLiteral bs) = bs
