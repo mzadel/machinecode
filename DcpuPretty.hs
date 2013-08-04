deb0787227b91e1464de514ee8990a3b2dbe3eab
Mark Zadel <zadel@music.mcgill.ca>
2013-08-04 15:30:50 -0400

    DcpuPretty: labeltostring: print hex rep'n along with integer value

--- DcpuPretty.hs
+++ DcpuPretty.hs
@@ -8,11 +8,14 @@ module DcpuPretty where
 import qualified DcpuSpecTables as Dcpu
 import CodeAst
 import BitList (bitstostring,bitsToWord16)
+import Text.Printf
 
 labeltostring :: Show a => Field (Dcpu.FieldType, a) -> String
 labeltostring (FieldVariable (Dcpu.RegA,label) bs) = "Register A: " ++ show label
 labeltostring (FieldVariable (Dcpu.RegB,label) bs) = "Register B: " ++ show label
-labeltostring (FieldVariable (Dcpu.RegADataWord,label) bs) = "Register A data: " ++ bitstostring bs ++ " (" ++ (show $ bitsToWord16 bs) ++ ")"
-labeltostring (FieldVariable (Dcpu.RegBDataWord,label) bs) = "Register B data: " ++ bitstostring bs ++ " (" ++ (show $ bitsToWord16 bs) ++ ")"
+labeltostring (FieldVariable (Dcpu.RegADataWord,label) bs) = "Register A data: " ++ bitstostring bs ++ " (" ++ (printf "0x%04x" value) ++ " == " ++ (show value) ++ ")"
+    where value = bitsToWord16 bs
+labeltostring (FieldVariable (Dcpu.RegBDataWord,label) bs) = "Register B data: " ++ bitstostring bs ++ " (" ++ (printf "0x%04x" value) ++ " == " ++ (show value) ++ ")"
+    where value = bitsToWord16 bs
 
 -- vim:sw=4:ts=4:et:ai:
