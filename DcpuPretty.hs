3e2ce60e9b134c50f966bb043de211565ce4dff9
Mark Zadel <zadel@music.mcgill.ca>
2013-08-04 15:38:15 -0400

    DcpuPretty: don't bother reprinting bit string

--- DcpuPretty.hs
+++ DcpuPretty.hs
@@ -13,9 +13,9 @@ import Text.Printf
 labeltostring :: Show a => Field (Dcpu.FieldType, a) -> String
 labeltostring (FieldVariable (Dcpu.RegA,label) bs) = "Register A: " ++ show label
 labeltostring (FieldVariable (Dcpu.RegB,label) bs) = "Register B: " ++ show label
-labeltostring (FieldVariable (Dcpu.RegADataWord,label) bs) = "Register A data: " ++ bitstostring bs ++ " (" ++ (printf "0x%04x" value) ++ " == " ++ (show value) ++ ")"
+labeltostring (FieldVariable (Dcpu.RegADataWord,label) bs) = "Register A data: " ++ (printf "0x%04x" value) ++ " == " ++ (show value)
     where value = bitsToWord16 bs
-labeltostring (FieldVariable (Dcpu.RegBDataWord,label) bs) = "Register B data: " ++ bitstostring bs ++ " (" ++ (printf "0x%04x" value) ++ " == " ++ (show value) ++ ")"
+labeltostring (FieldVariable (Dcpu.RegBDataWord,label) bs) = "Register B data: " ++ (printf "0x%04x" value) ++ " == " ++ (show value)
     where value = bitsToWord16 bs
 
 -- vim:sw=4:ts=4:et:ai:
