08b4c0f147eb0747a4f7e2ab2f60a5c7433c85d3
Mark Zadel <zadel@music.mcgill.ca>
2013-08-04 14:58:45 -0400

    Pretty: moved the bit manipulation functions into BitList

--- BitList.hs
+++ BitList.hs
@@ -16,7 +16,7 @@ export list." -}
 module BitList where
 
 import qualified Data.ByteString as B
-import Data.Word (Word8)
+import Data.Word (Word8,Word16)
 import Data.Bit  (Bit,fromBool)
 import Data.Bits (testBit)
 
@@ -39,5 +39,52 @@ bitsFromByteString :: B.ByteString -> [Bit]
 bitsFromByteString bs = case B.uncons bs of
     Nothing -> []
     Just (w, rest) -> bitsFromByte w ++ bitsFromByteString rest
+
+bitstostring :: [Bit] -> String
+bitstostring = concat . map show
+
+bitsToWord16 :: [Bit] -> Word16
+bitsToWord16 [b15,b14,b13,b12,b11,b10,b9,b8,b7,b6,b5,b4,b3,b2,b1,b0] =
+    fromIntegral b15 * 0x8000 +
+    fromIntegral b14 * 0x4000 +
+    fromIntegral b13 * 0x2000 +
+    fromIntegral b12 * 0x1000 +
+    fromIntegral b11 * 0x0800 +
+    fromIntegral b10 * 0x0400 +
+    fromIntegral b9  * 0x0200 +
+    fromIntegral b8  * 0x0100 +
+    fromIntegral b7  * 0x0080 +
+    fromIntegral b6  * 0x0040 +
+    fromIntegral b5  * 0x0020 +
+    fromIntegral b4  * 0x0010 +
+    fromIntegral b3  * 0x0008 +
+    fromIntegral b2  * 0x0004 +
+    fromIntegral b1  * 0x0002 +
+    fromIntegral b0  * 0x0001
+
+tohex :: [Bit] -> String
+tohex bs = concat $ map onechar (byfours bs)
+    where
+        byfours :: [Bit] -> [[Bit]]
+        byfours [] = []
+        byfours bs = a : byfours b
+            where
+                (a,b) = splitAt 4 bs
+        onechar (0:0:0:0:bs) = "0   "
+        onechar (0:0:0:1:bs) = "1   "
+        onechar (0:0:1:0:bs) = "2   "
+        onechar (0:0:1:1:bs) = "3   "
+        onechar (0:1:0:0:bs) = "4   "
+        onechar (0:1:0:1:bs) = "5   "
+        onechar (0:1:1:0:bs) = "6   "
+        onechar (0:1:1:1:bs) = "7   "
+        onechar (1:0:0:0:bs) = "8   "
+        onechar (1:0:0:1:bs) = "9   "
+        onechar (1:0:1:0:bs) = "a   "
+        onechar (1:0:1:1:bs) = "b   "
+        onechar (1:1:0:0:bs) = "c   "
+        onechar (1:1:0:1:bs) = "d   "
+        onechar (1:1:1:0:bs) = "e   "
+        onechar (1:1:1:1:bs) = "f   "
 
 -- vim:sw=4:ts=4:et:ai:
--- Pretty.hs
+++ Pretty.hs
@@ -9,58 +9,12 @@ import CodeAst
 import Data.Bit
 import Data.Word (Word16)
 import Data.Bits
+import BitList (bitstostring,bitsToWord16,tohex)
 
 -- given the code ast for one instruction, return a string that pretty prints it
 -- todo: split out the generic parts of this from the dcpu-specific parts
 
 labelcolumn = 50
-
-bitstostring :: [Bit] -> String
-bitstostring = concat . map show
-
-bitsToWord16 :: [Bit] -> Word16
-bitsToWord16 [b15,b14,b13,b12,b11,b10,b9,b8,b7,b6,b5,b4,b3,b2,b1,b0] =
-    fromIntegral b15 * 0x8000 +
-    fromIntegral b14 * 0x4000 +
-    fromIntegral b13 * 0x2000 +
-    fromIntegral b12 * 0x1000 +
-    fromIntegral b11 * 0x0800 +
-    fromIntegral b10 * 0x0400 +
-    fromIntegral b9  * 0x0200 +
-    fromIntegral b8  * 0x0100 +
-    fromIntegral b7  * 0x0080 +
-    fromIntegral b6  * 0x0040 +
-    fromIntegral b5  * 0x0020 +
-    fromIntegral b4  * 0x0010 +
-    fromIntegral b3  * 0x0008 +
-    fromIntegral b2  * 0x0004 +
-    fromIntegral b1  * 0x0002 +
-    fromIntegral b0  * 0x0001
-
-tohex :: [Bit] -> String
-tohex bs = concat $ map onechar (byfours bs)
-    where
-        byfours :: [Bit] -> [[Bit]]
-        byfours [] = []
-        byfours bs = a : byfours b
-            where
-                (a,b) = splitAt 4 bs
-        onechar (0:0:0:0:bs) = "0   "
-        onechar (0:0:0:1:bs) = "1   "
-        onechar (0:0:1:0:bs) = "2   "
-        onechar (0:0:1:1:bs) = "3   "
-        onechar (0:1:0:0:bs) = "4   "
-        onechar (0:1:0:1:bs) = "5   "
-        onechar (0:1:1:0:bs) = "6   "
-        onechar (0:1:1:1:bs) = "7   "
-        onechar (1:0:0:0:bs) = "8   "
-        onechar (1:0:0:1:bs) = "9   "
-        onechar (1:0:1:0:bs) = "a   "
-        onechar (1:0:1:1:bs) = "b   "
-        onechar (1:1:0:0:bs) = "c   "
-        onechar (1:1:0:1:bs) = "d   "
-        onechar (1:1:1:0:bs) = "e   "
-        onechar (1:1:1:1:bs) = "f   "
 
 fieldbits :: Field a -> [Bit]
 fieldbits (FieldLiteral bs) = bs
